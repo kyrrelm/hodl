143344fab303bf9371e7ba35e71db3b2410a3ebc
kyrremoe <kyrre.moe@bekk.no>
2017-11-18 11:14:14 +0100

    userResource

---
+++ resource/userResource.js
@@ -0,0 +1,30 @@
+const ObjectID = require('mongodb').ObjectID;
+
+const path = '/user';
+
+module.exports.register =  (router) => {
+
+  router.get(path, async function (ctx) {
+    ctx.body = await ctx.app.user.find().toArray();
+  });
+
+  router.get(`${path}/:id`, async (ctx) => {
+    ctx.body = await ctx.app.user.findOne({'_id': ObjectID(ctx.params.id)});
+  });
+
+  router.post(path, async function (ctx) {
+    ctx.body = await ctx.app.user.insert(ctx.request.body);
+  });
+
+  router.put(`${path}/:id`, async (ctx) => {
+    let documentQuery = {'_id': ObjectID(ctx.params.id)}; // Used to find the document
+    let valuesToUpdate = ctx.request.body;
+    ctx.body = await ctx.app.user.updateOne(documentQuery, valuesToUpdate);
+  });
+
+  router.delete(`${path}/:id`, async (ctx) => {
+    let documentQuery = {'_id': ObjectID(ctx.params.id)}; // Used to find the document
+    ctx.body = await ctx.app.user.deleteOne(documentQuery);
+  });
+
+};
--- server.js
+++ server.js
@@ -2,60 +2,30 @@ const Koa = require("koa");
 const Router = require("koa-router");
 const BodyParser = require("koa-bodyparser");
 const logger = require('koa-logger');
-const ObjectID = require("mongodb").ObjectID;
 const jwt = require("./jwt");
 const authResource = require('./resource/authResource.js');
+const userResource = require('./resource/userResource.js');
 
 const app = new Koa();
-
-const router = new Router();
-
-const secureRouter = new Router();
-secureRouter.use(jwt.errorHandler());
-secureRouter.use(jwt.jwt());
 
 app.use(logger());
 app.use(BodyParser());
 
 require("./mongo")(app);
 
-router.get("/", async function (ctx) {
-  ctx.body = {message: `Hello World!`}
-});
+const router = new Router();
+const secureRouter = new Router();
+secureRouter.use(jwt.errorHandler());
 
-router.post("/", async function (ctx) {
-  let name = ctx.request.body.name || "World";
-  ctx.body = {message: `Hello ${name}!`}
-});
+secureRouter.use(jwt.jwt());
 
 authResource.register(router);
+userResource.register(secureRouter);
 
-secureRouter.get("/user", async function (ctx) {
-  ctx.body = await ctx.app.user.find().toArray();
+router.get("/", async function (ctx) {
+  ctx.body = {message: `Endepunkter på /auth og /user`}
 });
 
-secureRouter.get("/user/:id", async (ctx) => {
-  ctx.body = await ctx.app.user.findOne({"_id": ObjectID(ctx.params.id)});
-});
-
-secureRouter.post("/user", async function (ctx) {
-  ctx.body = await ctx.app.user.insert(ctx.request.body);
-});
-
-secureRouter.put("/user/:id", async (ctx) => {
-  let documentQuery = {"_id": ObjectID(ctx.params.id)}; // Used to find the document
-  let valuesToUpdate = ctx.request.body;
-  ctx.body = await ctx.app.user.updateOne(documentQuery, valuesToUpdate);
-});
-
-secureRouter.delete("/user/:id", async (ctx) => {
-  let documentQuery = {"_id": ObjectID(ctx.params.id)}; // Used to find the document
-  ctx.body = await ctx.app.user.deleteOne(documentQuery);
-});
-
-secureRouter.get("/_ah/health", async function (ctx) {
-  ctx.body = {message: `Im good :)`}
-});
 
 app.use(router.routes()).use(router.allowedMethods());
 app.use(secureRouter.routes()).use(secureRouter.allowedMethods());
