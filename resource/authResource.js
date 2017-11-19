5bebb2efa12b5792c6a086a0f9d111d8f8164c96
kyrremoe <kyrre.moe@bekk.no>
2017-11-19 15:08:22 +0100

    Register and authenticate

--- resource/authResource.js
+++ resource/authResource.js
@@ -4,25 +4,14 @@ const path = '/auth';
 
 module.exports.register =  (router) => {
 
-  router.post(path, async (ctx) => {
+  router.post(`${path}/register`, async (ctx) => {
 
-    validateFields(ctx, { email: 'string', password: 'string' });
-
-    ctx.sanitize('email').trim();
-    ctx.sanitize('email').escape();
-    ctx.sanitize('password').trim();
-
-    ctx.checkBody('email').isEmail();
-    let errors = await ctx.validationErrors();
-
-    if (errors) {
-      ctx.throw(400, 'Invalid email');
-    }
+    validateUser(ctx);
 
     let email = ctx.request.body.email;
     let password = ctx.request.body.password;
 
-    const emailExists = await ctx.app.user.find({email: email}).limit(1).hasNext();
+    const emailExists = await ctx.app.user.find({ email }).limit(1).hasNext();
 
     if(emailExists) {
       ctx.throw(409, 'Email already in use');
@@ -33,11 +22,48 @@ module.exports.register =  (router) => {
     ctx.body = {
       token: secure.issueJwt({
         email,
-        role: "admin"
+        role: "user"
       })
     }
   });
 
+  router.post(path, async (ctx) => {
+
+    validateUser(ctx);
+
+    let email = ctx.request.body.email;
+    let password = ctx.request.body.password;
+
+    const user = await ctx.app.user.findOne({ email, password }, {email});
+
+    if(!user) {
+      ctx.throw(409, 'Incorrect email or password');
+    }
+
+    ctx.body = {
+      token: secure.issueJwt({
+        email: user.email,
+        role: "user"
+      })
+    }
+  });
+
+};
+
+const validateUser = async (ctx) => {
+
+  validateFields(ctx, { email: 'string', password: 'string' });
+
+  ctx.sanitize('email').trim();
+  ctx.sanitize('email').escape();
+  ctx.sanitize('password').trim();
+
+  ctx.checkBody('email').isEmail();
+  let errors = await ctx.validationErrors();
+
+  if (errors) {
+    ctx.throw(400, 'Invalid email');
+  }
 };
 
 
