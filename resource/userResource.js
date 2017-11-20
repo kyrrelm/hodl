5aa39ba85b1f5afc28551ef172c671889ff4d1e2
kyrremoe <kyrre.moe@bekk.no>
2017-11-20 17:07:26 +0100

    disabled most user endpoints, removed hash from user response

--- resource/userResource.js
+++ resource/userResource.js
@@ -5,26 +5,26 @@ const path = '/user';
 module.exports.register =  (router) => {
 
   router.get(path, async function (ctx) {
-    ctx.body = await ctx.app.user.find().toArray();
+    ctx.body = await ctx.app.user.find({}, { _id: true, email: true }).toArray();
   });
 
-  router.get(`${path}/:id`, async (ctx) => {
-    ctx.body = await ctx.app.user.findOne({'_id': ObjectID(ctx.params.id)});
-  });
+  // router.get(`${path}/:id`, async (ctx) => {
+  //   ctx.body = await ctx.app.user.findOne({'_id': ObjectID(ctx.params.id)});
+  // });
 
-  router.post(path, async function (ctx) {
-    ctx.body = await ctx.app.user.insert(ctx.request.body);
-  });
+  // router.post(path, async function (ctx) {
+  //   ctx.body = await ctx.app.user.insert(ctx.request.body);
+  // });
 
-  router.put(`${path}/:id`, async (ctx) => {
-    let documentQuery = {'_id': ObjectID(ctx.params.id)}; // Used to find the document
-    let valuesToUpdate = ctx.request.body;
-    ctx.body = await ctx.app.user.updateOne(documentQuery, valuesToUpdate);
-  });
+  // router.put(`${path}/:id`, async (ctx) => {
+  //   let documentQuery = {'_id': ObjectID(ctx.params.id)}; // Used to find the document
+  //   let valuesToUpdate = ctx.request.body;
+  //   ctx.body = await ctx.app.user.updateOne(documentQuery, valuesToUpdate);
+  // });
 
-  router.delete(`${path}/:id`, async (ctx) => {
-    let documentQuery = {'_id': ObjectID(ctx.params.id)}; // Used to find the document
-    ctx.body = await ctx.app.user.deleteOne(documentQuery);
-  });
+  // router.delete(`${path}/:id`, async (ctx) => {
+  //   let documentQuery = {'_id': ObjectID(ctx.params.id)}; // Used to find the document
+  //   ctx.body = await ctx.app.user.deleteOne(documentQuery);
+  // });
 
 };
