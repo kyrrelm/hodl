caad21b91902e304d349e589c70b1a86963a5fff
kyrremoe <kyrre.moe@bekk.no>
2017-11-21 19:42:50 +0100

    Tasks test heroku 2

--- tasks.js
+++ tasks.js
@@ -2,10 +2,6 @@ const fetch = require('node-fetch');
 
 module.exports.currencies = () => {
   updateCurrencies();
-  setInterval(function(){
-    console.log("HERE HERE");
-    //app.portfolio.insert({userId:"34", code:"TEST", amount:"123"});
-  }, 3000);
 
 };
 
