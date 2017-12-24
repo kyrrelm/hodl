84f7e1a8bd672de43b03cd662e39da18fdf9fc00
kyrremoe <kyrre.moe@bekk.no>
2017-12-24 01:12:11 +0100

    api race condition bug

--- hodl-backend/tasks.js
+++ hodl-backend/tasks.js
@@ -7,13 +7,14 @@ module.exports.currencies = (app) => {
 
 const updateCurrencies = (app) => {
   console.log("Updating currencies...");
-  fetch('https://api.coinmarketcap.com/v1/ticker/?convert=NOK&limit=10000')
+  fetch('https://api.coinmarketcap.com/v1/ticker/?convert=NOK&start=0&limit=10000')
       .then(res => res.json())
       .then(json => {
         const currencies = json
             .sort((a, b) => parseInt(a.rank) - parseInt(b.rank));
-        app.currency.deleteMany({});
-        app.currency.insertMany(currencies);
-        console.log("Done updating currencies");
+        app.currency.deleteMany({}).then(() => {
+          app.currency.insertMany(currencies);
+          console.log("Done updating currencies");
+        });
       }).catch(err => console.log(err));
 };
